4bc7dfbead05a3381c6fdfc8295c92d5d50847d6
joshuaquilala <joshuacquilala@gmail.com>
2021-01-18 15:49:06 +0800

    Initial commit

---
+++ app/src/main/java/com/example/capstone_bustap/Login.kt
@@ -0,0 +1,15 @@
+package com.example.capstone_bustap
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class Login : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        }
+    }
